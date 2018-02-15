b57ceaede9d515829424adbde58888ff593781e9
Jeremy Bennett <jeremylukebennett@comcast.net>
2018-02-15 14:47:33 -0600

    done

--- chaining.js
+++ chaining.js
@@ -1,12 +1,6 @@
 const integers = [23, 15, 6, 3, 11, 20, 18, 7, 21, 1, 29, 10, 12, 8];
 
-let result = integers.sort(function(first, second){return second - first;}).filter(function(n){return n <= 19;}).map(function(n){return( n + 2);}).reduce(currentTotal, next)
-
-
-
-
-
-;
+let result = integers.sort(function(first, second){return second - first;}).filter(function(n){return n <= 19;}).map(function(n){return( (n * 1.5) - 1);});
 
 
 console.log(result);
